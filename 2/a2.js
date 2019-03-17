f76440b2ff556e148208b9f695aa3153ac0ad8e0
AntonVoronezh <pstut@mail.ru>
2019-03-17 20:33:44 +0300

    2#3

---
+++ 2/a2.js
@@ -0,0 +1,7 @@
+const square = argNum => argNum * argNum;
+const square2 = argNum => argNum * argNum * argNum;
+const square3 = argNum => argNum * argNum * argNum * argNum;
+
+exports.square = square;
+exports.square2 = square2;
+exports.square3 = square3;
